9dc334c9f1ad56b8fa82d3573d6714056068e679
fatemeehterami <fateme.ehterami8214@gmail.com>
2023-01-20 21:26:12 +0330

    fifth commit :)) melobit / music adapter , music list update

--- app/src/main/java/com/shariaty/melobit/MainActivityMusic.java
+++ app/src/main/java/com/shariaty/melobit/MainActivityMusic.java
@@ -1,18 +1,36 @@
 package com.shariaty.melobit;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivityMusic extends AppCompatActivity {
+
+    private final List<MusicList> musicList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        View decodeView = getWindow().getDecorView();
+        int options = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        
+
         setContentView(R.layout.activity_music);
 
         final LinearLayout searchBtn = findViewById(R.id.searchBtn);
@@ -27,11 +45,41 @@ public class MainActivityMusic extends AppCompatActivity {
         musicRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            getMusicFiles();
+        }
+        else {
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                requestPermissions(new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE},11);
+            }
+            else{
+                getMusicFiles();
 
-
-
-
-
+            }
+        }
 
     }
+
+
+       private void getMusicFiles(){
+
+
+        }
+
+        @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,@NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            getMusicFiles();
+
+
+        }
+        else{
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+        }
+
+        }
+
+
 }
--- app/src/main/java/com/shariaty/melobit/MusicAdapter.java
+++ app/src/main/java/com/shariaty/melobit/MusicAdapter.java
@@ -1,31 +1,64 @@
 package com.shariaty.melobit;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.myViewHolder> {
+    private final List<MusicList> list;
+    private final Context context;
+
+    public MusicAdapter(List<MusicList> list, Context context) {
+        this.list  = list;
+        this.context = context;
+    }
+
+    @SuppressLint("InflateParams")
     @NonNull
     @Override
     public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new myViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.music_adapter_layout,null));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MusicAdapter.myViewHolder holder, int position) {
+        MusicList list2 = list.get(position);
 
+        if(list2.isPlaying())
+        {
+            holder.rootLayout.setBackgroundResource(R.drawable.round_back_blue_10);
+        }
+        else {
+            holder.rootLayout.setBackgroundResource(R.drawable.round_back_10);
+
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     static class myViewHolder extends RecyclerView.ViewHolder {
+        private final RelativeLayout rootLayout;
+        private final TextView title;
+        private final TextView artist;
+
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
+            rootLayout=itemView.findViewById(R.id.rootLayout);
+            title=itemView.findViewById(R.id.musicTitle);
+            artist=itemView.findViewById(R.id.musicArtist);
+
         }
     }
 }
